87102d47f1e12df1aca4d98d774abf261c2608ff
Say No More <itsmgsai@gmail.com>
2024-11-20 22:16:28 +0700

    update bookings

--- resources/views/components/menu/employee-menu.blade.php
+++ resources/views/components/menu/employee-menu.blade.php
@@ -52,11 +52,9 @@
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                             class="feather feather-users">
-                            <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                            <circle cx="9" cy="7" r="4"></circle>
-                            <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-                            <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                             class="feather feather-book">
+                            <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
+                            <path d="M4 4h16v16H6.5A2.5 2.5 0 0 1 4 17.5V4z"></path>
                         </svg>
                         <span>Bookings</span>
                     </div>
