04cd7c98595435c0bca69b6ec0d7dda3d883244d
Say No More <itsmgsai@gmail.com>
2024-11-20 20:24:52 +0700

    change footer and svg icons

--- resources/views/components/menu/custom-menu.blade.php
+++ resources/views/components/menu/custom-menu.blade.php
@@ -11,9 +11,9 @@
             </div>
             <div class="nav-item sidebar-toggle">
                 <div class="btn-toggle sidebarCollapse">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                         stroke-linejoin="round" class="feather feather-chevrons-left">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="feather feather-chevrons-left">
                         <polyline points="11 17 6 12 11 7"></polyline>
                         <polyline points="18 17 13 12 18 7"></polyline>
                     </svg>
@@ -21,24 +21,23 @@
             </div>
         </div>
         @if (!Request::is('collapsible-menu/*'))
-            <div class="profile-info">
-                <div class="user-info">
-                    <div class="profile-content">
-                        <h6 class="">{{ Auth::user()->name }}</h6>
-                    </div>
+        <div class="profile-info">
+            <div class="user-info">
+                <div class="profile-content">
+                    <h6 class="">{{ Auth::user()->name }}</h6>
                 </div>
             </div>
+        </div>
         @endif
         <ul class="list-unstyled menu-categories" id="accordionExample">
 
             <li class="menu {{ Request::routeIs('admin.home') ? 'active' : '' }}">
                 <a href="{{ route('admin.home') }}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                             stroke-linejoin="round" class="feather feather-calendar">
-                            <rect x="3" y="4" width="18" height="18" rx="2"
-                                  ry="2"></rect>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="feather feather-calendar">
+                            <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
                             <line x1="16" y1="2" x2="16" y2="6"></line>
                             <line x1="8" y1="2" x2="8" y2="6"></line>
                             <line x1="3" y1="10" x2="21" y2="10"></line>
@@ -51,14 +50,14 @@
                 <a href="{{ route('admin.employees.index') }}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                             class="feather feather-users">
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="feather feather-users">
                             <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
                             <circle cx="9" cy="7" r="4"></circle>
                             <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
                             <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
                         </svg>
-                        <span>Employees</span>
+                        <span>employees</span>
                     </div>
                 </a>
             </li>
@@ -66,12 +65,11 @@
                 <a href="{{ route('admin.room-types.index') }}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                             class="feather feather-users">
-                            <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                            <circle cx="9" cy="7" r="4"></circle>
-                            <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-                            <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="feather feather-bed">
+                            <path
+                                d="M3 7v10M21 7v10M5 7h14a2 2 0 0 1 2 2v6H3v-6a2 2 0 0 1 2-2zM7 7V5a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2">
+                            </path>
                         </svg>
                         <span>Room Types</span>
                     </div>
@@ -81,13 +79,12 @@
                 <a href="{{ route('admin.rooms.index') }}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                             class="feather feather-users">
-                            <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                            <circle cx="9" cy="7" r="4"></circle>
-                            <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-                            <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="feather feather-door">
+                            <rect x="6" y="2" width="12" height="20"></rect>
+                            <circle cx="15" cy="12" r="1"></circle>
                         </svg>
+
                         <span>Rooms</span>
                     </div>
                 </a>
